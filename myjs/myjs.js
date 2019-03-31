48bdd276a4d155d42e138911f76d92989894cd66
TsaiDong <a6209123456789@yahoo.com.tw>
2019-04-01 01:24:32 +0800

    履歷

---
+++ myjs/myjs.js
@@ -0,0 +1,13 @@
+var vm= new Vue({
+  el: "#app",
+  data:{
+       personalinfo:[{name:"蔡松翰"},
+                  {birth:"1992-10-21"},
+                  {from:"台灣"},
+                  {address:"彰化縣花壇鄉"},
+                  {tel:"0989283065"},
+                  {mail:"a6209123456789@yahoo.com.tw"}
+                 ]
+}
+  
+})
---
+++ test.php
@@ -0,0 +1,9 @@
+
+<html></html>
+<head>
+  <meta charset="UTF-8"/>
+  <title>Document</title>
+</head>
+<body>
+  <div class="test">123---<?php echo "hello1213212132";?></div>
+</body>
